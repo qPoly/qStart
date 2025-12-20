a4bfac0c73eee7e508eab9628aeec4cec3856430
RogierVC <rogiervancann@gmail.com>
2025-12-20 11:39:49 +0100

    Removed some tests

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -18,7 +18,7 @@ test('users can authenticate using the login screen', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $response->assertRedirect('/');
 });
 
 test('users can not authenticate with invalid password', function () {
